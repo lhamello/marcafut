562aae21e27434238c29663c3eadf0fdc3273b8a
Luiz Henrique Araujo Mello <luiz-mello@procergs.rs.gov.br>
2017-12-18 16:11:01 -0200

    Uma implementação de validação de e-mail.

---
+++ src/main/java/com/projects/lham/infraestrutura/definicao/ObjetoDeValor.java
@@ -0,0 +1,9 @@
+package com.projects.lham.infraestrutura.definicao;
+
+import java.io.Serializable;
+
+public class ObjetoDeValor implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}
