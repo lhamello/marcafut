e7df421b9b0da5a96f19d1bac2b9661095b5636b
Luiz H. A. Mello <lhamello@gmail.com>
2017-12-19 19:38:51 -0200

    Limpando o projeto.

--- src/main/java/com/projects/lham/infraestrutura/definicao/ObjetoDeValor.java
+++
@@ -1,9 +0,0 @@
-package com.projects.lham.infraestrutura.definicao;
-
-import java.io.Serializable;
-
-public class ObjetoDeValor implements Serializable {
-
-	private static final long serialVersionUID = 1L;
-
-}
--- src/main/java/com/projects/lham/infraestrutura/definicao/ServicoAplicacao.java
+++
@@ -1,9 +0,0 @@
-package com.projects.lham.infraestrutura.definicao;
-
-import java.io.Serializable;
-
-public abstract class ServicoAplicacao implements Serializable {
-
-	private static final long serialVersionUID = 1L;
-
-}
