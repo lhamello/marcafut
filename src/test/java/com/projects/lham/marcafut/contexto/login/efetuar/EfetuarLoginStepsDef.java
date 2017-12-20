6b4a5f0d4aa31890ea456c3044830dc6c61ea579
Luiz H. A. Mello <lhamello@gmail.com>
2017-12-19 23:27:11 -0200

    Descomentado testes.

--- src/test/java/com/projects/lham/marcafut/contexto/login/efetuar/EfetuarLoginStepsDef.java
+++ src/test/java/com/projects/lham/marcafut/contexto/login/efetuar/EfetuarLoginStepsDef.java
@@ -1,9 +1,10 @@
 package com.projects.lham.marcafut.contexto.login.efetuar;
 
-//import com.projects.lham.contexto.login.efetuar.ServicoEfetuarLogin;
-//import com.projects.lham.infraestrutura.excecao.CampoObrigatorioRuntimeException;
-//import com.projects.lham.infraestrutura.excecao.ServicoRuntimeException;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
+import com.projects.lham.marcafut.infraestrutura.AbstractServicoRuntimeException;
+import com.projects.lham.marcafut.infraestrutura.excecao.CampoObrigatorioRuntimeException;
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Entao;
 import cucumber.api.java.pt.Quando;
@@ -12,9 +13,8 @@ public class EfetuarLoginStepsDef {
 
     private String email = null;
     private String senha = null;
-    // private ServicoEfetuarLogin servicoEfetuarLogin = new
-    // ServicoEfetuarLogin();
-    // private ServicoRuntimeException servicoRuntimeException;
+    private ServicoEfetuarLogin servicoEfetuarLogin = new ServicoEfetuarLogin();
+    private AbstractServicoRuntimeException servicoRuntimeException;
 
     @Dado("^que nao preencho o campo	e-mail$")
     public void queNaoPreenchoOCampoEMail() throws Throwable {
@@ -28,17 +28,16 @@ public class EfetuarLoginStepsDef {
 
     @Quando("^tento logar no sistema$")
     public void tentoLogarNoSistema() throws Throwable {
-        // try {
-        // servicoEfetuarLogin.acessarSistema(email, senha);
-        // } catch (ServicoRuntimeException excecao) {
-        // servicoRuntimeException = excecao;
-        // }
+        try {
+            servicoEfetuarLogin.acessarSistema(email, senha);
+        } catch (AbstractServicoRuntimeException excecao) {
+            servicoRuntimeException = excecao;
+        }
     }
 
     @Entao("^deve ser exigido o preenchimento do campo email$")
     public void deveSerExigidoOPreenchimentoDoCampoEmail() throws Throwable {
-        // String mensagemErro = CampoObrigatorioRuntimeException.MENSAGEM_ERRO;
-        // assertThat(servicoRuntimeException.getMessage(),
-        // is(equalTo(String.format(mensagemErro, "E-mail"))));
+        String mensagemErro = CampoObrigatorioRuntimeException.MENSAGEM_ERRO;
+        assertThat(servicoRuntimeException.getMessage(), equalTo(String.format(mensagemErro, "E-mail")));
     }
 }
